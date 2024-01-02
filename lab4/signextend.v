4efa9cd232c2e6d86f07ea0ac88d19e96423f2ad
wendadawen <wendadawen@qq.com>
2024-01-02 11:30:27 +0800

    fifth stage v2

--- lab4/signextend.v
+++ lab4/signextend.v
@@ -1,13 +1,16 @@
 `timescale 1ns / 1ps
-
+`include "defines.vh"
 module signextend (
     input wire [5:0] opcode,
     input wire [5:0] funct,
     input wire [15:0] in,
-    output wire [31:0] out
+    output reg [31:0] out
 );
-    assign out = (opcode[5:2] == 4'b00_10)?
-                    {{16{in[15]}},in[15:0]}:
-                    {{16{1'b0}},in[15:0]};
+    always @(*) begin
+        case(opcode) 
+            `OP_ANDI, `OP_ORI, `OP_XORI, `OP_LUI: out <= {{16{1'b0}},in[15:0]};
+            default: out <=  {{16{in[15]}},in[15:0]};
+        endcase
+    end
 
 endmodule
